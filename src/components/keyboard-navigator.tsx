eeb0514492d33220185e988a862ffc21fe653077
r0binsahin <robin.sahin88@gmail.com>
2025-03-14 11:18:43 +0100

    handles numbers bigges than 9

--- src/components/keyboard-navigator.tsx
+++ src/components/keyboard-navigator.tsx
@@ -2,7 +2,7 @@
 
 import { Story } from '@/types';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 interface KeyboardNavigatorProps {
   stories: Story[];
@@ -10,27 +10,48 @@ interface KeyboardNavigatorProps {
 
 export const KeyboardNavigator = ({ stories }: KeyboardNavigatorProps) => {
   const router = useRouter();
+  const [input, setInput] = useState('');
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
       const key = event.key;
-      if (/^[1-9]$/.test(key)) {
-        const index = parseInt(key, 10) - 1;
-        if (index < stories.length) {
-          const story = stories[index];
-          if (story.url) {
-            window.open(story.url, '_blank');
-          } else {
-            router.push(`/${story.id}`);
-          }
-        }
+
+      if (/^\d$/.test(key)) {
+        setInput((prevInput) => {
+          const newInput = prevInput + key;
+
+          if (timeoutRef.current) clearTimeout(timeoutRef.current);
+
+          timeoutRef.current = setTimeout(() => {
+            const index = parseInt(newInput, 10) - 1;
+
+            if (index >= 0 && index < stories.length) {
+              const story = stories[index];
+
+              if (story.url) {
+                window.open(story.url, '_blank');
+              } else {
+                router.push(`/${story.id}`);
+              }
+            }
+
+            setInput('');
+          }, 500);
+
+          return newInput;
+        });
       }
     };
 
     document.addEventListener('keydown', handleKeyPress);
+
     return () => {
       document.removeEventListener('keydown', handleKeyPress);
+
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
     };
   }, [stories, router]);
+
   return null;
 };
