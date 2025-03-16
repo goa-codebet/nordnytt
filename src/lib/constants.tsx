00cd5adc233b4123d9bbecec96e42a02f120637d
Lucas Nordgren <65816176+lucasnordic@users.noreply.github.com>
2025-03-16 12:31:15 +0100

    Update hackernews API

---
+++ src/lib/constants.tsx
@@ -0,0 +1,5 @@
+const HN_API_BASE = "https://hacker-news.firebaseio.com/v0";
+const CACHE_TIME = 120;
+const STORIES_PER_PAGE = 10;
+
+export { HN_API_BASE, CACHE_TIME, STORIES_PER_PAGE };
--- src/services/hn.ts
+++ src/services/hn.ts
@@ -1,21 +1,49 @@
 import { Comment, Story } from "@/types";
+import { HN_API_BASE, CACHE_TIME, STORIES_PER_PAGE } from "@/lib/constants";
 
-export const getTopStories = async ():Promise<Story[]> => {
-  const topstories:number[] = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json", {
-    next: {
-      revalidate: 120
+// Fetch top story IDs
+export const getTopStoryIDs = async (): Promise<number[]> => {
+  try {
+    const res = await fetch(`${HN_API_BASE}/topstories.json`, {
+      next: { revalidate: CACHE_TIME },
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch top stories");
     }
-  }).then(res => res.json());
-  
-  return Promise.all(topstories.splice(0, 10).map(id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, {
-    next: {
-      revalidate: 120
-    }
-  }).then(res => res.json())))
+
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+// Fetch multiple stories by IDs
+export const getStoriesByIDs = async (ids: number[]): Promise<Story[]> => {
+  if (!ids.length) return [];
+  try {
+    const fetches = ids.map(id =>
+      fetch(`${HN_API_BASE}/item/${id}.json`, {
+        next: { revalidate: CACHE_TIME },
+      })
+        .then(res => res.ok ? res.json() : null)
+        .catch(error => {
+          console.error(error);
+          return null;
+        })
+    );
+
+    const stories = await Promise.all(fetches);
+    return stories.filter(story => story && story.id); // filter out null values
+  } catch (error) {
+    console.error("Error fetching stories:", error);
+    return [];
+  }
 }
 
-export const getStory = async (id:number):Promise<Story> => {
-  return await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, {
+export const getStory = async (id: number): Promise<Story> => {
+  return await fetch(`${HN_API_BASE}/item/${id}.json`, {
     next: {
       revalidate: 120
     }
@@ -23,8 +51,8 @@ export const getStory = async (id:number):Promise<Story> => {
     .then(res => res.json());
 }
 
-export const getComment = async (id:number):Promise<Comment> => {
-  return await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, {
+export const getComment = async (id: number): Promise<Comment> => {
+  return await fetch(`${HN_API_BASE}/item/${id}.json`, {
     next: {
       revalidate: 120
     }
