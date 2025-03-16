53d9a6f50f2b638d53c264009a1b5e93d217373a
Lucas Nordgren <65816176+lucasnordic@users.noreply.github.com>
2025-03-16 13:17:16 +0100

    Update StoriesPage & handle no stories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,43 @@
 import { getTopStoryIDs, getStoriesByIDs } from "@/services/hn";
 import { STORIES_PER_PAGE } from "@/lib/constants";
 import StoriesList from "@/app/components/StoriesList";
+import { Story } from "@/types";
 
-export default async function Home({
+const NO_STORIES_MESSAGE = "No stories available.";
+
+export default async function StoriesPage({
   searchParams,
 }: {
   searchParams: { page?: string };
 }) {
   let page = parseInt(searchParams.page || "1");
-  const storyIDs = await getTopStoryIDs();
+  const stories = await getStories(page, 0);
 
-  if (isNaN(page) || page < 1) page = 1; // Prevent invalid pages
-  if (!storyIDs.length) return <p>No stories available.</p>;
+  return (
+    <main>
+      {stories.length > 0 ? (
+        <StoriesList stories={stories} page={page} />
+      ) : (
+        <p className="text-center">{NO_STORIES_MESSAGE}</p>
+      )}
+    </main>
+  );
+}
+
+// function for getting the stories
+async function getStories(
+  page: number,
+  storyIDcount: number
+): Promise<Story[]> {
+  const storyIDs = await getTopStoryIDs();
+  if (!storyIDs.length) return [];
 
   const startIndex = (page - 1) * STORIES_PER_PAGE;
   const endIndex = startIndex + STORIES_PER_PAGE;
   const currentPageIDs = storyIDs.slice(startIndex, endIndex);
 
   const stories = await getStoriesByIDs(currentPageIDs);
+  if (!stories.length) return [];
 
-  return (
-    <main>
-      <StoriesList stories={stories} page={page} />
-    </main>
-  );
+  return stories;
 }
