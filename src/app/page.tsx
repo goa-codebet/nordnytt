d1177798ff58288d1285f759aec2055cb70f60b5
Jonas Nilsson <jn223ck@student.lnu.se>
2025-03-19 17:59:38 +0100

    implemented paginaton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,63 @@
-import { getTopStories } from "@/services/hn";
+"use client";
+import { useSearchParams } from "next/navigation"; 
+import { useState, useEffect } from "react";
+import { getAllStories } from "@/services/hn";
+import { Story } from "@/types";
 import Link from "next/link";
 
-export default async function Home() {
-  const topstories = await getTopStories();
+
+export default function Home() {
+  const searchParam = useSearchParams()
+
+  const currentPage = parseInt(searchParam.get("page") || "1")
+
+  const [stories, setStories] = useState<Story[]>([])
+  const [totalPages, setTotalPages] = useState(1)
+
+
+  useEffect(() => {
+    async function fetchStories() {
+      const { stories, totalPages } = await getAllStories(currentPage)
+      setStories(stories)
+      setTotalPages(totalPages)
+      
+    }
+    fetchStories();
+  },[currentPage])
 
   return (
     <main>
-      { topstories.map((story, index) => {
+      { stories.map((story, index) => {
         const url = story.url ? new URL(story.url) : null;
-
         return (
           <div className="leading-none mb-4" key={story.id}>
-            <a title={story.title} href={story.url || `/${story.id}`} target={url?.host ? "_blank" : ""} className="pb-1 whitespace-nowrap text-ellipsis overflow-hidden block font-bold visited:text-slate-500">{index+1}. {story.title}</a>
+            <a title={story.title} href={story.url || `/${story.id}`} target={url?.host ? "_blank" : ""} className="pb-1 whitespace-nowrap text-ellipsis overflow-hidden block font-bold visited:text-slate-500">{index + (currentPage - 1) * 10 +1}. {story.title}</a>
             <div className="text-xs text-slate-700">{url?.host ? `${url?.host} - ` : ''} <Link href={`/${story.id}`}>{story.score} poäng - {story.descendants || 'Inga'} kommentarer - {Math.floor(Date.now()/1000 - story.time)} sekunder sedan</Link></div>
           </div>
         )
       }) }
+        <Link href={`/?page=${currentPage - 1}`} passHref>
+          <button
+            disabled={currentPage === 1}
+            className={`px-4 py-2 text-white font-semibold rounded-lg transition-all duration-300
+              ${currentPage === 1 ? "bg-gray-300 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600"} `}
+            aria-label="Previous page">
+            Previous
+          </button>
+        </Link>
+      <span> Page {currentPage} of {totalPages} </span>
+      
+      <Link href={`/?page=${currentPage + 1}`} passHref>
+          <button
+            disabled={currentPage === totalPages}
+            className={`px-4 py-2 text-white font-semibold rounded-lg transition-all duration-300
+              ${currentPage === totalPages ? "bg-gray-300 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600"} `}
+            aria-label="Next page">
+            Next
+          </button>
+      </Link>
     </main>
   );
 }
+
+
--- src/services/hn.ts
+++ src/services/hn.ts
@@ -14,6 +14,27 @@ export const getTopStories = async ():Promise<Story[]> => {
   }).then(res => res.json())))
 }
 
+// we get all stories but we only return 10 of them depending on the page number that we send in also return total pages.
+export const getAllStories = async (page:number): Promise<{stories:Story[]; totalPages:number}> => {
+  const allstories:number[] = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json", {
+    next: {
+      revalidate: 120
+    }
+  }).then(res => res.json());
+
+  const totalStories = allstories.length
+  const totalPages = Math.ceil(totalStories / 10)
+
+  const stories:Story[] = await Promise.all(allstories.slice((page -1) * 10, page * 10).map(id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`,{
+      next: {
+        revalidate: 120
+      }
+    }).then(res => res.json())))
+
+  return {stories, totalPages}
+}
+
+
 export const getStory = async (id:number):Promise<Story> => {
   return await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, {
     next: {
@@ -31,3 +52,4 @@ export const getComment = async (id:number):Promise<Comment> => {
   })
     .then(res => res.json());
 }
+
