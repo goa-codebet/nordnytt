333c63d24b8dab0342071e3a27d88649ea580757
Jonathan Frey <jf223rf@student.lnu.se>
2025-03-31 17:47:37 +0200

    break out fetching and scrolling into hook

--- src/app/components/Stories.tsx
+++ src/app/components/Stories.tsx
@@ -1,74 +1,39 @@
 "use client";
 
-import { getTopStories } from "@/services/hn";
-import { Story } from "@/types";
 import Link from "next/link";
-import { useCallback, useEffect, useState } from "react";
+import useStories from "../hooks/useStories";
 
 export default function Stories() {
-  const [stories, setStories] = useState<Story[]>([]);
-  const [page, setPage] = useState<number>(1);
-
-  const getStories = useCallback(async (page: number) => {
-    const limit = 10;
-    const newStories = await getTopStories(limit, (page - 1) * limit);
-    setStories((prev) => {
-      return [...prev, ...newStories];
-    });
-  }, []);
-
-  useEffect(() => {
-    getStories(1);
-  }, [getStories]);
-
-  const handleScroll = useCallback(() => {
-    const bottom =
-      Math.ceil(window.innerHeight + window.scrollY) >=
-      document.documentElement.scrollHeight - 200;
-    if (bottom) {
-      setPage((prevPage) => {
-        const nextPage = prevPage + 1;
-        getStories(nextPage);
-        return nextPage;
-      });
-    }
-  }, [getStories]);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [handleScroll]);
+  const { stories, loading, error, observerTarget } = useStories();
 
   return (
     <main>
-      {stories
-        ? stories.map((story, index) => {
-            const url = story.url ? new URL(story.url) : null;
+      {stories.map((story, index) => {
+        const url = story.url ? new URL(story.url) : null;
 
-            return (
-              <div className="leading-none mb-4" key={story.id}>
-                <a
-                  title={story.title}
-                  href={story.url || `/${story.id}`}
-                  target={url?.host ? "_blank" : ""}
-                  className="pb-1 whitespace-nowrap text-ellipsis overflow-hidden block font-bold visited:text-slate-500"
-                >
-                  {index + 1}. {story.title}
-                </a>
-                <div className="text-xs text-slate-700">
-                  {url?.host ? `${url?.host} - ` : ""}{" "}
-                  <Link href={`/${story.id}`}>
-                    {story.score} poäng - {story.descendants || "Inga"}{" "}
-                    kommentarer - {Math.floor(Date.now() / 1000 - story.time)}{" "}
-                    sekunder sedan
-                  </Link>
-                </div>
-              </div>
-            );
-          })
-        : "no stories"}
+        return (
+          <div className="leading-none mb-4" key={story.id}>
+            <a
+              title={story.title}
+              href={story.url || `/${story.id}`}
+              target={url?.host ? "_blank" : ""}
+              className="pb-1 whitespace-nowrap text-ellipsis overflow-hidden block font-bold visited:text-slate-500"
+            >
+              {index + 1}. {story.title}
+            </a>
+            <div className="text-xs text-slate-700">
+              {url?.host ? `${url?.host} - ` : ""}{" "}
+              <Link href={`/${story.id}`}>
+                {story.score} poäng - {story.descendants || "Inga"} kommentarer
+                - {Math.floor(Date.now() / 1000 - story.time)} sekunder sedan
+              </Link>
+            </div>
+          </div>
+        );
+      })}
+      <div ref={observerTarget}></div>
+      {loading && <div>Laddar Stories...</div>}
+      {error && <div>{error}</div>}
     </main>
   );
 }
---
+++ src/app/hooks/useStories.ts
@@ -0,0 +1,53 @@
+import { getTopStories } from "@/services/hn";
+import { Story } from "@/types";
+import { useEffect, useRef, useState } from "react";
+
+export default function useStories() {
+  const [stories, setStories] = useState<Story[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [_page, setPage] = useState<number>(1);
+
+  const getStories = async (page: number) => {
+    if (loading) return;
+    setLoading(true);
+    const limit = 10;
+    try {
+      const newStories = await getTopStories(limit, (page - 1) * limit);
+      setStories((prev) => {
+        return [...prev, ...newStories];
+      });
+      setError(null);
+    } catch (error) {
+      setError("Kunde inte hämta stories");
+    }
+    setLoading(false);
+  };
+
+  const observerTarget = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setPage((prevPage) => {
+            getStories(prevPage);
+            const nextPage = prevPage + 1;
+            return nextPage;
+          });
+        }
+      },
+      { threshold: 1 }
+    );
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current);
+      }
+    };
+  }, [observerTarget]);
+
+  return { stories, loading, error, observerTarget };
+}
