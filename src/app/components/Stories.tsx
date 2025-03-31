c39e5f459023aefd8263b81ead45ee8722c8d3f6
Jonathan Frey <jf223rf@student.lnu.se>
2025-03-31 16:06:48 +0200

    refactor to use client component for stories

---
+++ src/app/components/Stories.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { getTopStories } from "@/services/hn";
+import { Story } from "@/types";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function Stories() {
+  const [stories, setStories] = useState<Story[] | null>(null);
+
+  useEffect(() => {
+    const fetchStories = async () => {
+      const topStories = await getTopStories();
+      setStories(topStories);
+    };
+
+    fetchStories();
+  }, []);
+
+  return (
+    <main>
+      {stories
+        ? stories.map((story, index) => {
+            const url = story.url ? new URL(story.url) : null;
+
+            return (
+              <div className="leading-none mb-4" key={story.id}>
+                <a
+                  title={story.title}
+                  href={story.url || `/${story.id}`}
+                  target={url?.host ? "_blank" : ""}
+                  className="pb-1 whitespace-nowrap text-ellipsis overflow-hidden block font-bold visited:text-slate-500"
+                >
+                  {index + 1}. {story.title}
+                </a>
+                <div className="text-xs text-slate-700">
+                  {url?.host ? `${url?.host} - ` : ""}{" "}
+                  <Link href={`/${story.id}`}>
+                    {story.score} poäng - {story.descendants || "Inga"}{" "}
+                    kommentarer - {Math.floor(Date.now() / 1000 - story.time)}{" "}
+                    sekunder sedan
+                  </Link>
+                </div>
+              </div>
+            );
+          })
+        : "no stories"}
+    </main>
+  );
+}
