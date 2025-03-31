36758bc7a0d914d87adf3dc117608387f0fd27f0
Jonathan Frey <jf223rf@student.lnu.se>
2025-03-31 16:01:57 +0200

    generalize getTopStories for pagination

--- src/services/hn.ts
+++ src/services/hn.ts
@@ -1,33 +1,41 @@
 import { Comment, Story } from "@/types";
 
-export const getTopStories = async ():Promise<Story[]> => {
-  const topstories:number[] = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json", {
-    next: {
-      revalidate: 120
+export const getTopStories = async (
+  limit: number = 10,
+  offset: number = 0
+): Promise<Story[]> => {
+  const topstories: number[] = await fetch(
+    "https://hacker-news.firebaseio.com/v0/topstories.json",
+    {
+      next: {
+        revalidate: 120,
+      },
     }
-  }).then(res => res.json());
-  
-  return Promise.all(topstories.splice(0, 10).map(id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, {
-    next: {
-      revalidate: 120
-    }
-  }).then(res => res.json())))
-}
+  ).then((res) => res.json());
 
-export const getStory = async (id:number):Promise<Story> => {
+  return Promise.all(
+    topstories.splice(offset, limit).map((id) =>
+      fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, {
+        next: {
+          revalidate: 120,
+        },
+      }).then((res) => res.json())
+    )
+  );
+};
+
+export const getStory = async (id: number): Promise<Story> => {
   return await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, {
     next: {
-      revalidate: 120
-    }
-  })
-    .then(res => res.json());
-}
+      revalidate: 120,
+    },
+  }).then((res) => res.json());
+};
 
-export const getComment = async (id:number):Promise<Comment> => {
+export const getComment = async (id: number): Promise<Comment> => {
   return await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, {
     next: {
-      revalidate: 120
-    }
-  })
-    .then(res => res.json());
-}
+      revalidate: 120,
+    },
+  }).then((res) => res.json());
+};
